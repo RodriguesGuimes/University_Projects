cd02839daf4bb9d7dd7ab1761974ec37f4a0f583
Guilherme Rodrigues <rodrigues.guimes@gmail.com>
2026-03-16 17:42:24 -0300

    Add files via upload

---
+++ 1Sem/Algoritmo_e_Prog/Aulas_praticas/Aula2_Variaveis.java
@@ -0,0 +1,33 @@
+public class Aula2_Variaveis {
+    public static void main (String [] args){
+        int a = 19, b = 4;
+
+        int soma = a + b;
+        int subtracao = a - b;
+        int multiplicacao = a * b;
+        int divisao = a / b;
+        int resto = a % b;
+
+        
+        double x = 7.5, y = 0.25;
+
+        double somaDouble = x + y;
+        double subtracaoDouble = x - y;
+        double multiplicacaoDouble = x * y;
+        double divisaoDouble = x / y;
+
+
+        System.out.println("soma = " + soma);
+        System.out.println("subtracao = " + subtracao);
+        System.out.println("multiplicacao = " + multiplicacao);
+        System.out.println("divisao = " + divisao);
+        System.out.println("resto = " + resto);
+
+        System.out.println("Agora vamos trabalhar com as variaveis double");
+
+        System.out.println("somaDouble = " + somaDouble);
+        System.out.println("subtracaoDouble = " + subtracaoDouble);
+        System.out.println("multiplicacaoDouble = " + multiplicacaoDouble);
+        System.out.println("divisaoDouble = " + divisaoDouble);
+    }
+}
---
+++ 1Sem/Algoritmo_e_Prog/Aulas_praticas/Exercicio.java
@@ -0,0 +1,14 @@
+//Declarar uma variavel inteira, uma double e uma string
+//A inteira representa a idade de uma pessoa
+//A string é o nome
+//A double é a altura
+
+public class Exercicio {
+    public static void main (String[] args){
+        int idade = 30;
+        String nome = "Guimes";
+        double altura = 1.68;
+
+        System.out.println(nome + "," + " você tem " + idade + " anos e altura de " + altura);
+    }
+}
